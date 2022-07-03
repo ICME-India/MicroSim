908ee542f9a6528016254dead73d159b5d027ec9
SwaroopSPradhan <swaroopspradhan.c2@gmail.com>
2022-07-03 13:01:46 +0530

    fixed minor errors

--- Dependency_checker/resources/pycalphad_pkgs.sh
+++ Dependency_checker/resources/pycalphad_pkgs.sh
@@ -19,10 +19,10 @@ pycalphad_status=$?
 if [ "$pycalphad_status" -eq 0 ]; then
 cond2=`echo "$pycalphad_ver_current == $pycalphad_ver_req" | bc`
  if [ "$cond2" -eq 1 ]; then
-      echo "The required Pycalphad Version: $PYCALPHAD_VERSION_REQUIRED exists \e[0;32m STATUS : OK \e[0m"
+      echo "The required Pycalphad Version: $1 exists \e[0;32m STATUS : OK \e[0m"
       echo Pycalphad Version : $pycalphad_version_current >> version_log.txt
  else
-      echo "Pycalphad version : "$pycalphad_version_current" exists but the required version is "$PYCALPHAD_VERSION_REQUIRED" Please install and try again. \e[1;36m STATUS : WRONG VERSION \e[0m"
+      echo "Pycalphad version : "$pycalphad_version_current" exists but the required version is "$1" Please install and try again. \e[1;36m STATUS : WRONG VERSION \e[0m"
   fi
 else
    echo "Pycalphad doesnt exist please install version 0.9.2 and try again  \e[0;31m STATUS : NOT FOUND \e[0m  "
