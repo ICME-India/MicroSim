e8adedd266acf961325c6edf8b1ac3537cad6dc5
tanmay2022 <tanmay2022@github.com>
2023-06-26 11:42:42 +0530

    added in file keys and fixed csv

--- resources/PP_tools/triple_point3.py
+++ resources/PP_tools/triple_point3.py
@@ -2,7 +2,7 @@ import numpy as np
 from vtk import vtkDataSetReader
 
 
-def quad_point(vtkData,dataset,infileDimension,timeItretion,Scalar_name):
+def triple_point3(vtkData,dataset,infileDimension,timeItretion,Scalar_name):
     overall_tp = np.empty(len(timeItretion) ,  dtype = object)
     
     for t in range(len(timeItretion)):
@@ -28,8 +28,8 @@ def quad_point(vtkData,dataset,infileDimension,timeItretion,Scalar_name):
             
         
         thisset = []
-        #triple_point = []
-        quad_point = []
+        triple_point = []
+        #quad_point = []
         x_list = []
         y_list = []
         z_list = []
@@ -47,18 +47,18 @@ def quad_point(vtkData,dataset,infileDimension,timeItretion,Scalar_name):
                     thisset.append(pf2[i][j][k])
                     thisset = set(thisset)
 
-                #if len(thisset) >= 3:
-                    #triple_point.append((i,j))
-                    #x_list.append(i)
-                    #y_list.append(j)
-                    #z_list.append(j)
-                    #overall_tp[t] = triple_point
-                if len(thisset) >= 4:
-                    quad_point.append((i,j))
+                if len(thisset) >= 3:
+                    triple_point.append((i,j))
                     x_list.append(i)
                     y_list.append(j)
                     z_list.append(j)
-                    overall_tp[t] = quad_point
+                    overall_tp[t] = triple_point
+                #if len(thisset) >= 4:
+                    #quad_point.append((i,j))
+                    #x_list.append(i)
+                    #y_list.append(j)
+                    #z_list.append(j)
+                    #overall_tp[t] = quad_point
 
                     # print("total triple points ", len(triple_point))
                     #print(triple_point)
