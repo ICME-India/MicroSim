bfd4711d6dccd1d6eedb34b8037067c0523e93ed
Anshika <120469502+Anshika-Kushwaha@users.noreply.github.com>
2023-07-20 16:55:17 +0530

    Delete swap_script.sh

--- swap_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sed -i 's/gnome-terminal/dbus-launch gnome-terminal/g' MicroSim.py
-sed -i 's/gnome-terminal/dbus-launch gnome-terminal/g' ./resources/functions/linux.py
-sed -i 's/gnome-terminal/dbus-launch gnome-terminal/g' ./resources/functions/windows.py
-sed -i 's/mpirun.mpich/mpirun/g' ./resources/functions/linux.py
-sed -i 's/mpirun.mpich/mpirun/g' ./resources/functions/windows.py
-mkdir bin
-
